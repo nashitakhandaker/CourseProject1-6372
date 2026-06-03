22010e53ccc2c11d2f2afe61c7477d932413d660
Nashita Khandaker <nashitakhandaker@Nashitas-MacBook-Pro.local>
2026-06-02 22:58:23 -0700

    pushing first part of EDA

---
+++ MSDS6372_Project-1.R
@@ -0,0 +1,44 @@
+library(ggplot2)
+hospital <- read.csv("HospitalDurations.csv")
+hospital
+
+# Checking for relationships as apart of the EDA 
+numeric_vars <- hospital[, c("Lgth.of.Sty",
+                             "Age",
+                             "Inf.Risk",
+                             "R.Cul.Rat",
+                             "R.CX.ray.Rat",
+                             "N.Beds",
+                             "Avg.Pat",
+                             "Avg.Nur",
+                             "Pct.Ser.Fac")]
+
+cor(numeric_vars)
+
+# Plot 1: Highest correlation scatterplot: Inf risk to Length of Stay
+ggplot(hospital, aes(x = Inf.Risk, y = Lgth.of.Sty)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(
+    title = "Infection Risk vs Length of Stay",
+    x = "Infection Risk",
+    y = "Length of Stay"
+  ) +
+  theme_minimal()
+
+# Plot 2: Interaction term: region plot
+ggplot(hospital,
+       aes(x = Inf.Risk,
+           y = Lgth.of.Sty,
+           color = factor(Region))) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(
+    title = "Infection Risk vs Length of Stay by Region",
+    x = "Infection Risk",
+    y = "Length of Stay",
+    color = "Region"
+  ) +
+  theme_minimal()
+
+ch
